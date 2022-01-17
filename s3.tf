0832d75db9be82e14c8e3e50d6f7c174347aa38c
NithiyaJoseph <68648111+NithiyaJoseph@users.noreply.github.com>
2022-01-17 23:48:44 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 provider "aws" {
  region="us-east-1"
 }
-resource "aws_s3_bucket" "terraform_state" {
- bucket = "terraform_state_nits"
+resource "aws_s3_bucket" "nits112bucket" {
+ bucket = "terraform_state_nits11"
  acl = "private"
 }
